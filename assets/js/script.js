23d62e8ecbdc27565a24c7d05e283f1bdd04caa2
katredford <katharine.redford@gmail.com>
2021-04-08 22:57:12 -0500

    add save button, work on local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+var catImg = document.createElement('img');
+
 fetch(`https://api.thecatapi.com/v1/images/search?api_key=dcf111f5-1f90-4914-9be9-fe70f80fdda3`)
     
 
@@ -19,14 +21,60 @@ fetch(`https://api.thecatapi.com/v1/images/search?api_key=dcf111f5-1f90-4914-9be
 
     //append img element to page
     catContainerEl.appendChild(catImg);
+
+    var saveCat = document.getElementById("saveCat")
+    
+    
+    saveCat.addEventListener("click", catSaved);
 });
 
-var button = document.getElementById("catGet")
+var getButton = document.getElementById("catGet")
 
-button.addEventListener("click", gotClicked);
+getButton.addEventListener("click", gotClicked);
 
 function gotClicked(e) {
     window.location.reload();
 }
 
-//notes notes notes
+
+
+function catSaved(e){
+console.log("got clicked")
+
+var catPlace = document.getElementById("catSave");
+catPlace.innerHTML= catImg;
+
+}
+
+//jokeapi
+fetch(`https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Pun?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single`)
+.then(function(response) {
+    return response.json();
+  })
+  .then(function(joke) {
+    console.log(joke);
+  });
+
+// // Conver the respionse to JSON
+// .then(function(response) {
+//     return response.json();
+// })
+
+// .then(function(response){
+//     //use "querySelector to get the ID of where the cat will be displayed"
+
+
+
+//     var jokeContainerEl = document.querySelector("#joke-container");
+
+//     // //create an img element
+//     var joke = document.createElement('p');
+
+//      //set src attribute to the image url from cat api response
+
+//     jokeText.setAttribute('src', joke);
+
+//     //append img element to page
+//      jokeContainerEl.appendChild(joke);
+// });
+
